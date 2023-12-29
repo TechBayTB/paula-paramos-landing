ab5761f4250a54619e65f128f2e3161de40a9e93
Bautista <bauticasanas@gmail.com>
2023-12-29 15:17:33 -0300

    Avances

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Education from './components/Education/Education';
 import Comments from './components/Comments/Comments';
 import WhyMe from './components/WhyMe/WhyMe';
 import Translation from './components/Translation/Translation';
+import DigitalSignature from './components/DigitalSignature/DigitalSignature';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <WhyMe />
       <Comments />
       <Translation />
+      <DigitalSignature />
     </LanguageContextProvider>
   );
 }
---
+++ src/components/DigitalSignature/components/DigitalSignatureUl.js
@@ -0,0 +1,12 @@
+
+
+const Reason = ({text}) => {
+
+    return (
+        <>
+            <li className="reason-item">{text}</li>
+        </>
+    );
+}
+
+export default Reason; 
