27ccf0ed6b47aaae16fbff36b7fb0f55bee444cd
nicogutik <nicogutik@gmail.com>
2024-01-03 18:16:14 -0300

    cambio iconos y whyme

--- src/components/Education/components/Title/Title.js
+++ src/components/Education/components/Title/Title.js
@@ -3,13 +3,16 @@ import LanguageContext from "../../../../Context/Language";
 import { LanguageTypes } from "../../../Types/LanguageTypes";
 import './Title.css'
 import { EducationEnglish, EducationSpanish } from "./TitleText";
+import educationPic from '../../../../assets/education.svg';
+
+
 const Title = ({title, titledesc, year}) => {
     const { language } = useContext(LanguageContext)
 
     return (
         <>
             <div className="education-title">
-                <img className="education-pic" src={require('../../../../assets/education.png')} alt='logo' />
+                <img className="education-pic" src={educationPic} alt="logo" />
                 <p className="titulo">{title}</p>
                 <p className="texto">{titledesc}</p>
                 <p className="texto">{year}</p>
