6ff40d9486bd64a5977be989deae6f392994b27e
Bautista <bauticasanas@gmail.com>
2024-01-04 15:06:59 -0300

    Enrolled

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,46 +1,102 @@
-import { useContext,useState } from "react";
+// import { useContext,useState } from "react";
+// import LanguageContext from "../../Context/Language";
+// import { LanguageTypes } from "../Types/LanguageTypes";
+// import './Comments.css'
+// import Message from "./components/Message/Message";
+// // import { EducationEnglish, EducationSpanish } from "./EducationText";
+// // import Title from "./components/Title/Title";
+
+// const Comments = () => {
+//     const { language } = useContext(LanguageContext);
+
+//     // Seguir igual como ' " " ' para seguir formato de "mensaje"
+//     const titlesData = [
+//         { comment: '"Los servicios de traducción provistos son de gran calidad y eficiencia. Muy recomendable"', name: "Leticia Arena"},
+//         { comment: '"Excelente servicio, muy profesional y eficiente. Muy recomendable"', name: "María Laura"},
+//         { comment: '"Muy buena atención y servicio. Muy recomendable"', name: "Ricasa S.A."},
+//     ];
+
+//     const [currentIndex, setCurrentIndex] = useState(0);
+
+//     const handleNext = () => {
+//         setCurrentIndex((prevIndex) => (prevIndex + 1) % titlesData.length);
+//     };
+
+//     const handlePrev = () => {
+//         setCurrentIndex((prevIndex) => (prevIndex - 1 + titlesData.length) % titlesData.length);
+//     };
+
+//     return (
+//         <>
+//             <div className="Comments" id="Comments">
+//                 <div className="comments-container">
+//                     <button onClick={handlePrev}>&lt;</button>
+//                     <Message
+//                         comment={titlesData[currentIndex].comment}
+//                         name={titlesData[currentIndex].name}
+//                     />
+//                     <button onClick={handleNext}>&gt;</button>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// }
+
+
+// export default Comments; 
+
+import { useContext, useState } from "react";
 import LanguageContext from "../../Context/Language";
 import { LanguageTypes } from "../Types/LanguageTypes";
-import './Comments.css'
+import "./Comments.css";
 import Message from "./components/Message/Message";
-// import { EducationEnglish, EducationSpanish } from "./EducationText";
-// import Title from "./components/Title/Title";
 
 const Comments = () => {
-    const { language } = useContext(LanguageContext);
+  const { language } = useContext(LanguageContext);
 
-    // Seguir igual como ' " " ' para seguir formato de "mensaje"
-    const titlesData = [
-        { comment: '"Los servicios de traducción provistos son de gran calidad y eficiencia. Muy recomendable"', name: "Leticia Arena"},
-        { comment: '"Excelente servicio, muy profesional y eficiente. Muy recomendable"', name: "María Laura"},
-        { comment: '"Muy buena atención y servicio. Muy recomendable"', name: "Ricasa S.A."},
-    ];
+  const titlesData = [
+    {
+      comment: '"Los servicios de traducción provistos son de gran calidad y eficiencia. Muy recomendable"',
+      name: "Leticia Arena",
+    },
+    {
+      comment: '"Excelente servicio, muy profesional y eficiente. Muy recomendable"',
+      name: "María Laura",
+    },
+    {
+      comment: '"Muy buena atención y servicio. Muy recomendable"',
+      name: "Ricasa S.A.",
+    },
+  ];
 
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    const handleNext = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % titlesData.length);
-    };
+  const handleIndicatorClick = (index) => {
+    setCurrentIndex(index);
+  };
 
-    const handlePrev = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + titlesData.length) % titlesData.length);
-    };
+  return (
+    <>
+      <div className="Comments" id="Comments">
+        <div className="comments-container">
+          <button onClick={() => setCurrentIndex((prevIndex) => (prevIndex - 1 + titlesData.length) % titlesData.length)}>
+            &lt;
+          </button>
+          <Message comment={titlesData[currentIndex].comment} name={titlesData[currentIndex].name} />
+          <button onClick={() => setCurrentIndex((prevIndex) => (prevIndex + 1) % titlesData.length)}>&gt;</button>
+        </div>
+        <div className="indicators-container">
+          {titlesData.map((_, index) => (
+            <span
+              key={index}
+              className={index === currentIndex ? "indicator active" : "indicator"}
+              onClick={() => handleIndicatorClick(index)}
+            ></span>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-            <div className="Comments" id="Comments">
-                <div className="comments-container">
-                    <button onClick={handlePrev}>&lt;</button>
-                    <Message
-                        comment={titlesData[currentIndex].comment}
-                        name={titlesData[currentIndex].name}
-                    />
-                    <button onClick={handleNext}>&gt;</button>
-                </div>
-            </div>
-        </>
-    );
-}
-
-
-export default Comments; 
+export default Comments;
---
+++ src/components/Enrolled/Enrolled.js
@@ -0,0 +1,19 @@
+
+import './Enrolled.css'
+
+
+const Enrolled = () => {
+    
+    return (
+        <>
+            <div className="Enrolled" id="Enrolled">
+                <img src={require(`../../assets/`)} alt="img-enrolled" className="img-enrolled"/>
+                <img src={require(`../../assets/`)} alt="img-enrolled" className="img-enrolled"/>
+                <img src={require(`../../assets/`)} alt="img-enrolled" className="img-enrolled"/>
+            </div>
+        </>
+    );
+}
+
+
+export default Enrolled; 
