0c305f32d9be7c6922eab92c0610de369da6a708
maxicassarino <maximocassarino@gmail.com>
2024-01-30 11:05:53 -0300

    Avances - Maxi

--- src/components/Translation/TranslationText.js
+++ src/components/Translation/TranslationText.js
@@ -1,20 +1,14 @@
 export const translationsTextES= [
     {text: '“La traducción es lo que transforma todo para que nada cambie”. – Günter  Grass'},
-    {text: "Desde el año 2002, me desempeño como traductora independiente y mi experiencia abarca una amplia variedad de textos:"},
-    {text: "Documentos personales: certificados de nacimiento, pasaportes."},
-    {text: "Documentos académicos: certificados analíticos, diplomas."},
-    {text: "Documentos legales: contratos, poderes."},
-    {text: "Documentos técnicos y científicos: manuales, tesis."},
+    {text: 'Desde 2002, realizo traducciones del Inglés al Español y del Español al Inglés sobre:'},
+    {text: "Documentaciones técnicas, personales, judiciales y académicas."},
     {text: "También realizo traducciones de textos relacionados con psicología, turismo, bienestar general, desarrollo personal, literatura y marketing."},
 ];
 
 export const translationsTextEN= [
     {text: '“Translation is that with transforms everything so that nothing changes.” – Günter  Grass '},
     {text: "Since 2002, I have been dedicated to independent translation, with expertise in a diverse range of texts:"},
-    {text: "Personal Documents: birth certificates, passports."},
-    {text: "Academic Documents: academic transcripts, diplomas."},
-    {text: "Legal Documents: agreements, powers-of-attorneys."},
-    {text: "Technical and Scientific Documents: manuals, theses."},
+    {text: "Personal, Technical, Scientific, Academic and Legal Documents."},
     {text: "Additionally, I work with texts related to psychology, tourism, general well-being, personal development, literature, and marketing."},
 ];
 
