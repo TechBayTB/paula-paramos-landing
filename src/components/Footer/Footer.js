5dcdf2791a0982474021f3981214243ea1e3a08d
maxicassarino <maximocassarino@gmail.com>
2024-02-05 16:41:14 -0300

    Avances - Maxi

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,62 +11,54 @@ const Footer = () => {
     }
 
     return (
-        <>
-            <footer class="footer">
-                <div class="footer-top-container">
-                    <div class="footer-top">
-                        <a href='https://www.aati.org.ar/aati' target="_blank"><img class="img-footer" src={require(`../../assets/matricula1.jpg`)}></img></a>
-                        <a href='https://sanisidro.traductorespba.org.ar/traductor/paramos-paula-andrea/' target="_blank"><img class="img-footer" src={require(`../../assets/matricula2.jpg`)}></img></a>
-                        <a href='https://www.traductores.org.ar/traductor/paramos-paula-andrea/' target="_blank"><img class="img-footer" src={require(`../../assets/matricula3.jpg`)}></img></a>
-                    </div>
+        <>  
+            <div class="footer-top-container">
+                <div class="footer-top">
+                    <a href='https://www.aati.org.ar/aati' target="_blank"><img class="img-footer" src={require(`../../assets/matricula1.jpg`)}></img></a>
+                    <a href='https://sanisidro.traductorespba.org.ar/traductor/paramos-paula-andrea/' target="_blank"><img class="img-footer" src={require(`../../assets/matricula2.jpg`)}></img></a>
+                    <a href='https://www.traductores.org.ar/traductor/paramos-paula-andrea/' target="_blank"><img class="img-footer" src={require(`../../assets/matricula3.jpg`)}></img></a>
                 </div>
-                <div class="main-footer-container">
-                    <div class="main-footer">
-                        {/* <div class="footer-left">
+            </div>
+            <div id='footerBackground'>
+                <div class="footer">
+                    <ul>
+                        <li id='footerTechbay'>
                             <h2>PAULA PÁRAMOS</h2>
-                            <p id="p-footer">{language === LanguageTypes.SPANISH ? FooterSpanish.text : FooterEnglish.text}</p>
-                        </div> */}
-                        <ul class="footer-right">
-                            <li id='footerPaula'>
-                                <h2 class="footer-titles" id="h2_footer_resources">PAULA PÁRAMOS</h2>
-                                <ul class="footer-box">
-                                    <li><p>{language === LanguageTypes.SPANISH ? FooterSpanish.text : FooterEnglish.text}</p></li>
-                                </ul>
-                            </li>
-                            <li className='footers'>
-                                <h2 class="footer-titles" id="h2_footer_resources">SERVICIOS</h2>
-                                <ul class="footer-box">
-                                    <li><p onClick={() => handleNavigation('Translation')}>Traducciones</p></li>
-                                    <li><p onClick={() => handleNavigation('Courses')} >Cursos</p></li>
-                                    <li><p onClick={() => handleNavigation('Firma')}>Firma Digital</p></li>
-                                    <li><p onClick={() => handleNavigation('Firma')}>Certificación</p></li>
-                                </ul>
-                            </li>
-                            <li className='footers'>
-                                <h2 class="footer-titles" id="h2_footer_us">SOBRE MÍ</h2>
-                                <ul class="footer-box">
-                                    <li><p onClick={() => handleNavigation('about-me')}>¿Quien Soy?</p></li>
-                                    <li><p onClick={() => handleNavigation('education')}>Mi educación</p></li>
-                                    <li><p onClick={() => handleNavigation('WhyMe')}>¿Por qué elegirme?</p></li>
-                                    <li><p onClick={() => handleNavigation('contact')}>Contáctame</p></li>
-                                </ul>
-                            </li>
-                            <li className='footers'>
-                                <h2 class="footer-titles" id="h2_footer_contact">CONTACTO</h2>
-                                <ul class="footer-box">
-                                    <li><p>paulaparamos@yahoo.com.ar</p></li>
-                                    <li><p >+54 9 15  5471  7868</p></li>
-                                    <li><p>Paula Páramos</p></li>
-                                    <li><p>Buenos Aires, Argentina</p></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <div class="footer-bottom">
-                            <p id="rights">Powered by TechBay</p>
-                        </div>
+                            <p>{language === LanguageTypes.SPANISH ? FooterSpanish.text : FooterEnglish.text}</p>
+                        </li>
+                        <li>
+                            <h2>SERVICIOS</h2>
+                            <div className='footerGrid'>
+                                <p style={{cursor: "pointer"}} onClick={() => handleNavigation('Translation')}>Traducciones</p>
+                                <p style={{cursor: "pointer"}} onClick={() => handleNavigation('Courses')}>Cursos</p>
+                                <p style={{cursor: "pointer"}} onClick={() => handleNavigation('Firma')}>Firma Digital</p>
+                                <p style={{cursor: "pointer"}} onClick={() => handleNavigation('Firma')}>Certificación</p>
+                            </div>
+                        </li>
+                        <li>
+                            <h2>SOBRE MÍ</h2>
+                            <div className='footerGrid'>
+                                <p style={{cursor: "pointer"}} onClick={() => handleNavigation('about-me')}>¿Quien Soy?</p>
+                                <p style={{cursor: "pointer"}} onClick={() => handleNavigation('education')}>Mi educación</p>
+                                <p style={{cursor: "pointer"}} onClick={() => handleNavigation('WhyMe')}>¿Por qué elegirme?</p>
+                                <p style={{cursor: "pointer"}} onClick={() => handleNavigation('contact')}>Contáctame</p>
+                            </div>
+                        </li>
+                        <li>
+                            <h2>CONTACTO</h2>
+                            <div className='footerGrid'>
+                                <p>Paula Páramos</p>
+                                <p>+54 9 15  5471  7868</p>
+                                <p>paulaparamos@yahoo.com.ar</p>
+                                <p>Buenos Aires, Argentina</p>
+                            </div>
+                        </li>
+                    </ul>
+                    <div id='footerLogos'>
+                        <p>Powered by TechBay</p>
                     </div>
                 </div>
-            </footer>
+            </div>
         </>
     );
 }
